2ae2e1aaf55950631a914b0b7c4f59cb8bf8c11c
zw-99 <zw-99@users.noreply.github.com>
2018-09-21 08:52:54 +0800

    删除无用代码

--- src/zw/automusic/Main.java
+++ src/zw/automusic/Main.java
@@ -17,59 +17,28 @@ public class Main {
 		String tc = "退出.mp3";
 		
 		XdSound xdSound = new XdSound(path + xd, 1000);
-		OpenAnimalsSound openAnimalsSound = new OpenAnimalsSound(path + dkcdw, 13500);
+		OpenAnimalsSound openAnimalsSound = new OpenAnimalsSound(path + dkcdw, 15500);
 		StartSound startSound = new StartSound(path + start, 9000);
 		ExitSound exitSound = new ExitSound(path + tc, 9000);
 		UnknowSound unknowSound = new UnknowSound(path + bzd, 9000);
 		LionSound lionSound = new LionSound(path + sz, 9000);
-		//对 xdSound 对象进行包装，执行一个完成的小度猜动物到结束的流程
+		//对 xdSound 对象进行包装，执行一个完整的小度猜动物流程
 		xdSound.decrate(openAnimalsSound);
 		openAnimalsSound.decrate(startSound);
 		startSound.decrate(lionSound);
 		lionSound.decrate(unknowSound);
-		unknowSound.decrate(exitSound);
+		unknowSound.decrate(exitSound); 
 		
 		try {
 			//开始播放
-			xdSound.playSound();
+			for(int i = 0; i < 5; i++) {
+				xdSound.playSound();
+			}
+			
 		} catch (FileNotFoundException | JavaLayerException | InterruptedException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		}
-		
-		
-		
-//		AutoPlay autoPlay = null;
-//		try {
-//			for(int i = 0; i < 6; i++) {
-//				autoPlay = new AutoPlay(new File(path + File.separator + xd));
-//				autoPlay.play();
-//				Thread.sleep(1000);
-//				autoPlay = new AutoPlay(new File(path + File.separator + dkcdw));
-//				autoPlay.play();
-//				Thread.sleep(13500);
-//				autoPlay = new AutoPlay(new File(path + File.separator + start));
-//				autoPlay.play();
-//				Thread.sleep(9000);
-//				autoPlay = new AutoPlay(new File(path + File.separator + bzd));
-//				autoPlay.play();
-//				Thread.sleep(9000);
-//				autoPlay = new AutoPlay(new File(path + File.separator + tc));
-//				autoPlay.play();
-//				Thread.sleep(9000);
-//			}
-//		} catch (InterruptedException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		} catch (FileNotFoundException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		} catch (JavaLayerException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}
-		
-		
+		}	
 	}
 
 }
